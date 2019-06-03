ce490ff01919fdcc32ba133199a7417fcce27715
TimothyHare <cordellth@yahoo.com>
2019-06-02 20:11:52 -0400

    Update 4

--- client/src/components/CourseDetail.js
+++ client/src/components/CourseDetail.js
@@ -1,3 +1,4 @@
+//Johnny louifils walked me through creating this
 import React from "react";
 import axios from "axios";
 import { NavLink } from "react-router-dom"; 
@@ -12,7 +13,115 @@ class CourseDetail extends Component {
           course: [],
           user: []
         };
+        this.handleDelete = this.handleDelete.bind(this); 
       }
+
+      componentDidMount() {
+        const {
+          match: { params }
+        } = this.props;  
+      
+        axios
+          .get(`http://localhost:5000/api/courses/${params.id}`)
+          .then(results => {
+    
+            this.setState({
+    
+              course: results.data,
+              user: results.data.user
+            });
+    
+          });
+      }
+
+      handleDelete() {
+        const {
+          match: { params },
+          history
+        } = this.props;
+    
+        axios
+          .delete(`http://localhost:5000/api/courses/${params.id}`, {
+            auth: {
+              username: window.localStorage.getItem("Email"),
+              password: window.localStorage.getItem("Password")
+            }
+          })
+          .then(() => {
+            history.push("/");
+          });
+      }
+
+      render() {
+        const { course, user } = this.state;
+        const isLoggedIn = localStorage.getItem("IsLoggedIn");
+        const UserId = JSON.parse(localStorage.getItem("UserId"));
+        
+        return (//JSX inside
+          <div>
+            <div className="actions--bar">
+              <div className="bounds">
+                <div className="grid-100">
+                  <span>
+                    {(isLoggedIn && user._id === UserId) ? ( 
+                      <span>
+                        <NavLink
+                          to={`/courses/${course._id}/update`}
+                          className="button"
+                        >
+                          Update Course
+                        </NavLink>
+                        <NavLink
+                          to={"#"}
+                          className="button"
+                          onClick={this.handleDelete}
+                        >
+                          Delete Course
+                        </NavLink>
+                      </span>
+                    ) : null} 
+                  </span>
+                  <NavLink
+                    to="/"
+                    className="button button-secondary"
+                    href="index.html"
+                  >
+                    Return to List
+                  </NavLink>
+                </div>
+              </div>
+            </div>
+            <div className="bounds course--detail">
+              <div className="course--header">
+                <h4 className="course--label">Course</h4>
+                <h3 className="course--title">{course.title}</h3>
+                <p>
+                  This course was created by: {user.firstName} {user.lastName}
+                </p>
+                <div className="course--description">
+                   <ReactMarkdown source={course.description} /> 
+                </div>
+              </div>
+              <div className="grid-25 grid-right">
+                <div className="course--stats">
+                  <ul className="course--stats--list">
+                    <li className="course--stats--list--item">
+                      <h4>Estimated Time</h4>
+                      <h3>{course.estimatedTime} hours</h3>
+                    </li>
+                    <li className="course--stats--list--item">
+                      <h4>Materials Needed</h4>
+                      <ul>
+                       <ReactMarkdown source={` * ${course.materialsNeeded}`} /> 
+                      </ul>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        );
+      } 
 
 }
 
--- client/src/components/UpdateCourse.js
+++ client/src/components/UpdateCourse.js
@@ -1,4 +1,130 @@
+//Johnny Louifils helped me create this file
 import React from 'react';
 import  axios  from "axios";
 
-class UpdateCourse extends Component {}
+class UpdateCourse extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+        course: [],
+        user: [],
+        title: '', 
+        description: '',
+        estimatedTime: '',
+        materialsNeeded: '',
+        errors: []
+        };
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleCancel = this.handleCancel.bind(this);
+    }
+
+    change = (event) => {
+        this.setState({
+        [event.target.name]: event.target.value
+        })
+    }
+
+    handleSubmit = (e) => {
+        const {match: { params }} = this.props;
+        e.preventDefault();
+          axios ({
+              method: 'put',
+              url: `http://localhost:5000/api/courses/${params.id}`,
+              auth: {
+                username: window.localStorage.getItem('Email'),
+                password: window.localStorage.getItem('Password')
+             },
+              data: {
+                  title: this.state.title,
+                  description: this.state.description,
+                  estimatedTime: this.state.estimatedTime,
+                  materialsNeeded: this.state.materialsNeeded
+                  }
+          }).then(response => { 
+            if (response.status === 204) {
+              alert("Update Accomplished");
+              this.props.history.push("/");
+            } else {
+              throw new Error();
+            }
+          })
+          .catch(err => {
+            console.log("CATCH =", err.response.data.errors);
+            this.setState({
+              errors: err.response.data.errors
+            });
+          });
+      };
+  
+      handleCancel = (evt) => {
+          var { history } = this.props;
+          var { course } = this.state;
+          evt.preventDefault();
+          history.push(`/courses/${course._id}`)
+      }
+      componentDidMount() {
+          const {match: { params }} = this.props;
+          axios
+        .get(`http://localhost:5000/api/courses/${params.id}`)
+        .then(results => {
+            this.setState({
+                course: results.data,
+                user: results.data.user
+            });
+        });
+  
+      }
+  
+      render() {
+        
+     const errors = this.state.errors; 
+     const errorList = errors.map((error) =>
+       <li key={error.toString()}>{error}</li>);
+       const { course, user } = this.state;
+  
+        return ( 
+         <div>
+         <hr />
+         <div className="bounds course--detail">
+           <h1>Update Course</h1>
+           <div className="validation-errors">
+               <ul>{errorList}</ul>
+             </div>
+           <div>
+             <form onSubmit={ this.handleSubmit}>
+               <div className="grid-66">
+                 <div className="course--header">
+                   <h4 className="course--label">Course</h4>
+                   <div><input id="title" name="title" type="text" className="input-title course--title--input" placeholder="Course title..." defaultValue={course.title}  onChange={e => this.change(e)} /></div>
+                   <p>By {user.firstName} {user.lastName}</p>
+                 </div>
+                 <div className="course--description">
+                   <div><textarea id="description" name="description"  placeholder={this.state.course.description} onChange={e => this.change(e)}/> </div>
+                 </div>
+               </div>
+               <div className="grid-25 grid-right">
+                 <div className="course--stats">
+                   <ul className="course--stats--list">
+                     <li className="course--stats--list--item">
+                       <h4>Estimated Time</h4>
+                       <div><input id="estimatedTime" name="estimatedTime" type="text" className="course--time--input" placeholder="Hours" defaultValue={this.state.course.estimatedTime} onChange={e => this.change(e)} /></div>
+                     </li>
+                     <li className="course--stats--list--item">
+                       <h4>Materials Needed</h4>
+                       <div><textarea id="materialsNeeded" name="materialsNeeded" placeholder={this.state.course.materialsNeeded}  onChange={e => this.change(e)} /></div>
+                     </li>
+                   </ul>
+                 </div>
+               </div>
+               <div className="grid-100 pad-bottom"><button className="button" type="submit">Update Course</button><button className="button button-secondary" onClick={this.handleCancel}>Cancel</button></div>
+             </form>
+           </div>
+         </div>
+       </div> 
+      );
+     } 
+
+}
+
+export default UpdateCourse;
